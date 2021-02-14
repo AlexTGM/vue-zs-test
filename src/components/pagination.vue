3765ee00ab634fccd035ba222cd9d59a880fefbd
a.mineev <alexandrmineev@gmail.com>
2021-02-14 21:39:49 +0300

    add pagination icons

--- src/components/pagination.vue
+++ src/components/pagination.vue
@@ -3,14 +3,16 @@ div
     a.is-primary.m-r-10(
         @click='$emit("previous")',
         :class='{ disabled: !params.has_previous }'
-    ) <-
+    ) 
+        font-awesome-icon(:icon='["fas", "arrow-left"]', size='lg')
 
-    span.is-opaque {{ params.from }} - {{ params.to }} из {{ params.count }}
+    span.is-opaque {{ params.from }} — {{ params.to }} из {{ params.count }}
 
     a.is-primary.m-l-10(
         @click='$emit("next")',
         :class='{ disabled: !params.has_next }'
-    ) ->
+    ) 
+        font-awesome-icon(:icon='["fas", "arrow-right"]', size='lg')
 </template>
 
 <script>
